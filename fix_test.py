dd36c80e701231f06afa6b62daa2c3259df42568
Mziana <289339226+Mziana@users.noreply.github.com>
2026-08-29 15:16:23 +0700

    chore: cleanup temporary python scripts

--- fix_test.py
+++
@@ -1,5 +0,0 @@
-with open("tests/are/test_phase5_preflight_invariants.py", "r") as f:
-    c = f.read()
-c = c.replace("self.assertEqual(res.details[\"gate_status\"], \"BORDERLINE\")", "self.assertEqual(res.details[\"gate_status\"], \"FAIL\")")
-with open("tests/are/test_phase5_preflight_invariants.py", "w") as f:
-    f.write(c)
